684d76aade5084072dba41a6020fc0430e44b6ea
Sanju <khadkasanju046@gmail.com>
2025-12-08 21:50:47 -0800

    Updated UI and input fields

--- App.js
+++ App.js
@@ -2,41 +2,75 @@ import React, { useState } from 'react';
 import { View, Text, TextInput, StyleSheet } from 'react-native';
 
 export default function App() {
-  const [name, setName] = useState(''); // state to store input 123
-
-  // Added comment to check new feature branch to test sub branch
-  
-
+  const [firstName, setFirstName] = useState(''); // state to store input 
+  const [lastName, setLastName] = useState(''); 
+  const [age, setAge] = useState('');
+ 
   return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Enter your name:</Text>
+    <View style={styles.flow}>
+       {/* First Name Row */}
+      <View style={{ flexDirection: 'row', alignItems: 'center', marginBottom: 10 }}>
+        <Text style={styles.title}>First Name :</Text>
+        <TextInput
+        style={styles.input}
+        placeholder="Enter your first name"
+        value={firstName}
+        onChangeText={text => setFirstName(text)}
+      />
+      {/* <Text style={styles.output}>Hello, {name || 'Jhon'}!</Text> */}
+    </View>
+
+    {/* Last Name Row */}
+    <View style={{ flexDirection: 'row', alignItems: 'center', marginBottom: 10 }}>
+      <Text style={styles.title}>Last Name :</Text>
       <TextInput
         style={styles.input}
-        placeholder="Type here"
-        value={name}
-        onChangeText={text => setName(text)}
+        placeholder="Enter your last name"
+        value={lastName}
+        onChangeText={text => setLastName(text)}
       />
-      <Text style={styles.output}>Hello, {name || 'stranger'}!</Text>
     </View>
+    {/* Age Row */}
+    <View style={{ flexDirection: 'row', alignItems: 'center', marginBottom: 10 }}>
+      <Text style={styles.title}>Age :</Text>
+      <TextInput
+        style={styles.input}
+        placeholder="Enter your age"
+        value={age}
+        onChangeText={text => setAge(text)}
+      />
+    </View>
+    {/* output */}
+    <Text style={styles.output}>
+      Hello, {firstName || 'Nia '} {lastName || 'Sharma'} you are {age || '25'} years old.</Text>
+    
+    </View>
+    
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
+  flow: {
     padding: 20,
-    backgroundColor: '#fff',
+      },
+  title: { 
+    fontSize: 20,
+    color: 'blue', 
+    fontWeight: 'bold',
+    width: 150,     // Fixed width so text aligns properly
   },
-  title: { fontSize: 20, marginBottom: 10 },
   input: {
     borderWidth: 1,
-    borderColor: 'gray',
-    width: '80%',
-    padding: 10,
-    marginBottom: 20,
+    borderColor: '#ccc',
     borderRadius: 5,
+    padding: 10,
+    color: 'blue',
+    foreweight: 'bold',
+    fontSize: 18,
+    // flex: 1,        // Make input take available space
   },
-  output: { fontSize: 24, color: 'blue' },
+  output: { fontSize: 20, 
+    fontWeight: 'bold', 
+    color: 'red' },
+  
 });
